9aa535fd20052a79d75e02d16e82bfc40ee1c24e
Hal Newton <hal.newton@gmail.com>
2011-10-21 10:47:55 -0700

    added site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,6 +9,18 @@ describe PagesController do
     end
   end
 
+    it "should be successful" do
+      get 'home'
+      response.should have_selector("title",
+            :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+  
+  it "should have a non-blank body" do
+    get 'home'
+    response.body.should_not =~ /<body>\s*<\/body>/
+  end
+  
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
@@ -16,4 +28,11 @@ describe PagesController do
     end
   end
 
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+  
 end
